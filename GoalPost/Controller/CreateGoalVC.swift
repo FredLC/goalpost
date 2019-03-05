8653af3a18215339b879bd9efa9e8f3d542f1032
fredlc <fredlefevre@outlook.com>
2019-03-05 17:20:02 -0500

    Implemented extension to bind view to keyboard

--- GoalPost/Controller/CreateGoalVC.swift
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -15,10 +15,13 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    var goalType: GoalType = .shortTerm
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
@@ -26,9 +29,15 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func shortTermButtonPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func longTermButtonPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
