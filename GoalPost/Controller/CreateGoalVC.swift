074b5fd4f3a16217ba9ac688eaffa64a12d479f0
fredlc <fredlefevre@outlook.com>
2019-03-05 16:13:01 -0500

    Built CreateGoal UI

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,37 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Fred Lefevre on 2019-03-05.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shortTermButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    
+}
