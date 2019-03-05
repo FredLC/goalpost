dffc91d8da95c68a64d4e798d9728a53eb6a0a25
fredlc <fredlefevre@outlook.com>
2019-03-05 14:25:00 -0500

    Created GoalCell outlets and configuration

---
+++ GoalPost/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  GoalPost
+//
+//  Created by Fred Lefevre on 2019-03-05.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescription: UILabel!
+    @IBOutlet weak var goalType: UILabel!
+    @IBOutlet weak var goalProgress: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        goalDescription.text = description
+        goalType.text = type
+        goalProgress.text = String(describing: goalProgressAmount)
+    }
+    
+}
