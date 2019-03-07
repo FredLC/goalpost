f98cdcf9b789d3c81a6cfaefb84b5281495212bc
fredlc <fredlefevre@outlook.com>
2019-03-06 19:09:33 -0500

    Saved goal data to persistent store and properly dismissed to GoalsVC

--- GoalPost/Controller/FinishGoalVC.swift
+++ GoalPost/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -16,22 +17,48 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     var goalDescription: String!
     var goalType: GoalType!
     
-    func initData(description: String, type: GoalType) {
-        self.goalDescription = description
-        self.goalType = type
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         createGoalButton.bindToKeyboard()
         pointsTextField.delegate = self
     }
     
-    @IBAction func backButtonPressed(_ sender: Any) {
+    func initData(description: String, type: GoalType) {
+        self.goalDescription = description
+        self.goalType = type
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
         
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("Successfully saved data!")
+            completion(true)
+        } catch {
+            debugPrint(error.localizedDescription)
+            completion(false)
+        }
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismissDetail()
     }
     
     @IBAction func createGoalButtonPressed(_ sender: Any) {
+        if pointsTextField.text != "" {
+            save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
 }
